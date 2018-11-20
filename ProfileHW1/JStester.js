3efed234f6d148a5b8c048a94727043caf7e3ecc
Alex Kehoe <a.kehoe.umt@gmail.com>
2018-11-19 19:01:51 -0800

    Getting closer, added more service pages

---
+++ ProfileHW1/JStester.js
@@ -0,0 +1,24 @@
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
--- ProfileHW1/Portfolio.js
+++ ProfileHW1/Portfolio.js
@@ -2,8 +2,13 @@ $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
 
-  // carousel
+  carousel
   $('#carousel-example-generic').on('slid.bs.carousel', function () {
     $(".carousel-item.active:nth-child(" + ($(".carousel-inner .carousel-item").length -1) + ") + .carousel-item").insertBefore($(".carousel-item:first-child"));
     $(".carousel-item.active:last-child").insertBefore($(".carousel-item:first-child"));
 }); 
+
+$('#myCarousel').carousel({
+  interval: 40000
+});
+
